a4175b88d92d3ef3cae2a5036af5ae48d5aacedd
Daniel Villalobos del Baño <daniel.villalobosdel@gmail.com>
2020-11-06 13:40:57 +0100

    🚧  New main side

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,9 +3,19 @@
     <div class="grid grid-cols-1 md:grid-cols-7 content">
       <div class="container col-span-1 md:col-span-3 md:h-screen w-full">
         <p class="text-center text-6xl font-hairline">Fagolambda</p>
-        <p class="text-center text-2xl">La nieve está mal</p>
-        <p class="text-center text-2xl">Dije claramente rojo, no bermellón</p>
-        <p class="text-center text-2xl">Eres parte de la cornisa Julio</p>
+        <div class="flex flex-col justify-around items-center">
+          <div class="flex flex-row">
+            <p class="text-center text-2xl side-element">¿Qué es Fagolambda?</p>
+          </div>
+          <div class="flex flex-row side-element">
+            <p class="text-center text-2xl">
+              Sobre mi, Lorena
+            </p>
+          </div>
+          <div class="flex flex-row side-element mb-12">
+            <p class="text-center text-2xl">Mi trabajo de fin de grado</p>
+          </div>
+        </div>
       </div>
       <div class="container col-span-4 md:h-screen w-full overflow-auto side">
         <slot />
@@ -18,11 +28,16 @@
 .layout {
   max-width: 100%;
   font-family: "Inter", sans-serif;
-  background-color: #1B97AD !important;
+  background-color: #1b97ad !important;
   color: white;
 }
 
-.side{
-  background-color: #e9e9e9;
+.side {
+  background-color: #ececec;
 }
+
+.side-element{
+  margin-top: 10vh;
+}
+
 </style>
