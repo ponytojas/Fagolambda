d80d95e7edd787fb9b6422e8994d97f5492f43a7
Daniel Villalobos del Baño <daniel.villalobosdel@gmail.com>
2020-11-06 13:47:59 +0100

    🚸  All the card should be the link

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="w-8/12 h-64 rounded overflow-hidden card">
-      <g-link :to="post.path">
-
-    <div class="px-6 py-8">
-      <div class="font-bold text-2xl font-hairline mb-2">
-        <h2 v-html="post.title" />
+  <g-link :to="post.path">
+    <div class="w-8/12 h-64 rounded overflow-hidden card">
+      <div class="px-6 py-8">
+        <div class="text-2xl font-hairline mb-2">
+          <h2 v-html="post.title" />
+        </div>
+        <p
+          class="text-gray-700 text-base whitespace-normal"
+          v-html="post.acf.subtitle"
+        ></p>
       </div>
-      <p
-        class="text-gray-700 text-base whitespace-normal"
-        v-html="post.acf.subtitle"
-      ></p>
     </div>
-    </g-link>
-  </div>
+  </g-link>
 </template>
 
 <script>
