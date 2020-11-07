70dad070f9d63bd5f1647c4a1ba5682984d2577e
Daniel Villalobos del Baño <daniel.villalobosdel@gmail.com>
2020-11-08 00:12:15 +0100

    ➕  New package for search

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import DefaultLayout from "~/layouts/Default.vue";
-import '~/assets/Inter/inter.css'
+import "~/assets/Inter/inter.css";
+import Autocomplete from "@trevoreyre/autocomplete-vue";
+import "@trevoreyre/autocomplete-vue/dist/style.css";
 
 export default function(Vue) {
   Vue.component("Layout", DefaultLayout);
+  Vue.use(Autocomplete);
 }
