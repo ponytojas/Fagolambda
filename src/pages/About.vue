0fdd45e6334a61b81ef3f404ae0a5e19611df0a1
Daniel Villalobos del Baño <daniel.villalobosdel@gmail.com>
2020-12-06 17:29:04 +0100

    🎨  Added images

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -6,12 +6,21 @@
         <span class="font-normal">m</span>i,
         <span class="font-normal">L</span>orena
       </p>
+
       <div
-        class="flex flex-col align-center items-center justify-center mt-8 mb-8 px-6 md:px-32 py-24 h-auto"
+        class="flex flex-col align-center items-center justify-center mt-8 mb-8 px-6 md:px-32 py-10 h-auto"
       >
-        <div
-          class="text-justify prose sm:prose lg:prose-lg xl:prose-xl WPcontent"
-        >
+        <g-image
+          src="../assets/profile.jpg"
+          @mouseover="
+            showingImage = showingImage == firtsRoute ? secondRoute : firtsRoute
+          "
+          width="250"
+          height="250"
+          class="rounded-full mb-10"
+        />
+
+        <div class="text-justify prose sm:prose lg:prose-lg xl:prose-xl">
           <p>
             Desde muy pequeña, cuando me preguntaban qué quería ser lo tenía muy
             claro: Bióloga.
@@ -38,6 +47,10 @@
     </div>
   </Layout>
 </template>
+
+<script>
+export default {};
+</script>
 
 <style>
 .side {
