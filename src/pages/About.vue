7146745877876cac1045cf74b612720a586fca4b
Daniel Villalboos <daniel.villalobosdel@gmail.com>
2020-12-28 18:59:57 +0100

    💄  Change profile image

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -11,7 +11,7 @@
         class="flex flex-col align-center items-center justify-center mt-8 mb-8 px-6 md:px-32 py-10 h-auto"
       >
         <g-image
-          src="../assets/profile.jpg"
+          src="../assets/profile.png"
           width="250"
           height="250"
           class="rounded-full mb-10"
