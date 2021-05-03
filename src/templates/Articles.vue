434a44381a099ebf0a78870c11c2001a519abf5b
ponytojas <daniel.villalobosdel@gmail.com>
2021-05-03 20:18:28 +0200

    ✨  Added youtube video as iframe

--- src/templates/Articles.vue
+++ src/templates/Articles.vue
@@ -1,6 +1,8 @@
 <template>
   <Layout>
-    <div class="w-full flex flex-col mx-auto items-center lg:w-10/12 2xl:w-7/12"></div>
+    <div
+      class="w-full flex flex-col mx-auto items-center lg:w-10/12 2xl:w-7/12"
+    ></div>
     <p
       class="text-center text-4xl lg:text-6xl font-semibold mt-8 gradient-text-articles-1 pb-4"
       v-html="$page.article.title"
@@ -47,18 +49,41 @@
           v-if="$page.article.update != 'None'"
           class="w-full h-auto text-gray-600 border-l-2 border-green-200 bg-gray-100 py-4 px-2 text-md text-left rounded-r-xl mb-10"
         >
-         <div class="px-4 ">
+          <div class="px-4">
             <p>
-            <span class="text-lg mb-12">Actualización del artículo</span> <br />
-            <span v-html="$page.article.update"></span>
-          </p>
-         </div>
+              <span class="text-lg mb-12">Actualización del artículo</span>
+              <br />
+              <span v-html="$page.article.update"></span>
+            </p>
+          </div>
         </div>
 
         <div
           class="text-justify max-w-none w-full prose prose-lg lg:prose-xl content"
           v-html="$page.article.content"
         />
+        <div
+          v-if="$page.article.youtube != ''"
+          class="mx-auto w-full h-96"
+        >
+          <iframe
+          class="mx-auto"
+          height="100%" width="100%"
+            :src="$page.article.youtube"
+          >
+          </iframe>
+        </div>
+
+        <template v-if="$page.article.tags.length">
+          <div class="px-6 pt-4 pb-2">
+            <span
+              v-for="tag in $page.article.tags"
+              :key="tag.id"
+              class="tag inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-800 mr-2 mb-2"
+              >#{{ tag }}</span
+            >
+          </div>
+        </template>
       </div>
     </div>
   </Layout>
@@ -76,6 +101,7 @@ query ($path: String!) {
     subtitle
     thumbnail
     tags
+    youtube
   }
 }
 </page-query>
@@ -87,6 +113,11 @@ export default {
       title: this.$page.article.title,
     };
   },
+  computed: {
+    betterTags: function () {
+      return this.$pages.article.tags;
+    },
+  },
 };
 </script>
 
@@ -97,10 +128,6 @@ export default {
   -webkit-background-clip: text;
   background-clip: text;
   -webkit-text-fill-color: transparent;
-}
-
-iframe {
-  width: 100% !important;
 }
 
 .tag {
