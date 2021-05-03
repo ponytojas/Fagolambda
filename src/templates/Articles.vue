1d5b1b229acc4294e2f6bac521a910a63e167433
ponytojas <daniel.villalobosdel@gmail.com>
2021-05-03 13:28:02 +0200

    🚚  Rename template

---
+++ src/templates/Articles.vue
@@ -0,0 +1,109 @@
+<template>
+  <Layout>
+    <div class="w-full flex flex-col mx-auto items-center lg:w-7/12"></div>
+    <p
+      class="text-center text-4xl lg:text-6xl font-semibold mt-8 gradient-text-articles-1 pb-4"
+      v-html="$page.article.title"
+    />
+    <div
+      class="mx-auto flex flex-col justify-center content-center align-middle items-center pb-10 w-full rounded-xl lg:w-6/12 border-0 lg:border shadow-none lg:shadow-xl"
+    >
+      <g-image
+        v-if="$page.article.thumbnail != '/None'"
+        :src="$page.article.thumbnail"
+        class="mb-10 rounded-xl rounded-b-none"
+        width="2"
+        height="2"
+        fit="fill"
+      />
+      <div
+        class="mx-auto flex flex-col justify-center content-center align-middle items-center w-full px-10 lg:px-12"
+      >
+        <p
+          class="prose prose-xl prose-green sm:prose-lg lg:prose-xl xl:prose-2xl text-center font-normal mb-2"
+          style="color: rgb(16, 185, 129)"
+          v-if="$page.article.subtitle"
+          v-html="$page.article.subtitle"
+        />
+
+        <p class="text-black">
+          Un artículo escrito por:
+          <span
+            v-html="$page.article.author"
+            class="prose prose-xl font-thin text-gray-700 mb-2"
+          />
+        </p>
+
+        <p class="italic text-black mb-12">
+          Tiempo aprox. de lectura:
+          <span
+            class="font-thin text-gray-700"
+            v-html="$page.article.time_to_read"
+          ></span
+          ><span class="font-thin text-gray-700"> minutos</span>
+        </p>
+
+        <div
+          class="text-justify max-w-none w-full prose prose-lg lg:prose-xl content"
+          v-html="$page.article.content"
+        />
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query ($path: String!) {
+  article: articles (path: $path) {
+    id
+    title
+    author
+    content
+    time_to_read
+    subtitle
+    thumbnail
+    tags
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$page.article.title,
+    };
+  },
+};
+</script>
+
+<style>
+.gradient-text-articles-1 {
+  text-transform: uppercase;
+  background: linear-gradient(to right, #813bf6 40%, #6d287b 100%);
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+iframe {
+  width: 100% !important;
+}
+
+.tag {
+  background-color: #ffb300;
+}
+
+.content {
+  color: #484855 !important;
+}
+
+img {
+  margin: 0 auto;
+  border-radius: 10px;
+}
+
+/* increase header size after 600px */
+@media all and (min-width: 600px) {
+}
+</style>
