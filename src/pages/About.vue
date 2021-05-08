5c25d1a9f56a81266a74797b693db2d02478680e
ponytojas <daniel.villalobosdel@gmail.com>
2021-05-08 12:56:53 +0200

    💄  Update About and About Fago

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,64 +1,68 @@
 <template>
   <Layout>
-     <p class="text-center text-4xl lg:text-6xl font-semibold mt-8 gradient-text-about pb-4">Sobre mi, Lorena</p>
-     <div
-      class="mx-auto px-4 flex flex-col justify-center content-center align-middle items-center mt-20 pb-10 lg:mt-10 w-full rounded-xl lg:w-8/12 border-0 lg:border shadow-none lg:shadow-xl"
+    <p
+      class="text-center text-4xl lg:text-6xl font-semibold mt-8 gradient-text-about pb-4"
     >
-      <div
-        class="flex flex-col align-center items-center justify-center mt-8 mb-8 px-6 md:px-32 py-10 h-auto"
-      >
-        <g-image
-          src="../assets/profile.png"
-          width="250"
-          height="250"
-          class="rounded-full mb-10"
-        />
+      Sobre mi, Lorena
+    </p>
+    <div
+      class="mx-auto flex flex-col justify-center content-center align-middle items-center pb-10 w-full rounded-xl 2xl:w-6/12 lg:w-11/12 border-0 2xl:border shadow-none 2xl:shadow-xl"
+    >
+      <g-image
+        src="../assets/profile.png"
+        width="250"
+        height="250"
+        class="rounded-full mb-10 mt-10"
+      />
 
-        <div class="text-justify prose sm:prose lg:prose-lg xl:prose-xl">
-          <p>
-            Desde muy pequeña, cuando me preguntaban qué quería ser lo tenía muy
-            claro: Bióloga.
-          </p>
-          <p>
-            Algunos años después conseguí hacerlo realidad. Durante la carrera
-            me di cuenta de lo fascinante que es la biología y decidí enfocar
-            mis estudios en la Neurobiología.
-          </p>
-          <p>
-            Actualmente estoy realizando prácticas externas en un laboratorio de
-            neurobiología, en la Universidad de Alcalá, enfocado en la
-            neurofisiología de la visión.
-          </p>
-          <p>
-            Además, estoy realizando mi Trabajo de Fin de Grado:
-            <span class="italic font-semibold"
-              >"Evaluación de la respuesta fisiológica de células nerviosas
-              sometidas a la acción de ligandos fotosensibles y estímulos
-              lumínicos"</span
-            >, basándome en el estudio de mis prácticas externas en las que
-            realizo <span class="font-semibold">E</span>lectroretinografía y
-            <span class="font-bold">P</span>atch
-            <span class="font-bold">C</span>lamp.
-          </p>
-        </div>
-        <div class="flex flex-row">
-          <button
-            class="mt-12 shadow button-btn sm:text-lg xl:text-xl focus:outline-none text-white px-4 py-2 sm:px-4 xl:py-2 xl:px-4 rounded mx-2"
-            type="button"
+      <div
+        class="mx-auto flex flex-col justify-center content-center align-middle items-center w-full px-10 lg:px-12"
+      >
+        <p class="text-justify max-w-none w-full prose prose-lg lg:prose-xl content">
+          Desde muy pequeña, cuando me preguntaban qué quería ser lo tenía muy
+          claro: Bióloga.
+        </p>
+        <br />
+        <p class="text-justify max-w-none w-full prose prose-lg lg:prose-xl content">
+          Algunos años después conseguí hacerlo realidad. Durante la carrera me
+          di cuenta de lo fascinante que es la biología y decidí enfocar mis
+          estudios en la Neurobiología.
+        </p>
+        <br />
+        <p class="text-justify max-w-none w-full prose prose-lg lg:prose-xl content">
+          Actualmente estoy realizando prácticas externas en un laboratorio de
+          neurobiología, en la Universidad de Alcalá, enfocado en la
+          neurofisiología de la visión.
+        </p>
+        <br />
+        <p class="text-justify max-w-none w-full prose prose-lg lg:prose-xl content">
+          Además, estoy realizando mi Trabajo de Fin de Grado:
+          <span class="italic font-semibold"
+            >"Evaluación de la respuesta fisiológica de células nerviosas
+            sometidas a la acción de ligandos fotosensibles y estímulos
+            lumínicos"</span
+          >, basándome en el estudio de mis prácticas externas en las que
+          realizo <span class="font-semibold">Electroretinografía</span> y
+          <span class="font-bold">Patch Clamp</span>.
+        </p>
+      </div>
+      <div class="flex flex-row">
+        <button
+          class="mt-12 shadow button-btn sm:text-lg xl:text-xl focus:outline-none text-white px-4 py-2 sm:px-4 xl:py-2 xl:px-4 rounded mx-2"
+          type="button"
+        >
+          <g-link to="~/assets/CV-Lorena-Biologia-min.pdf" target="_blank"
+            >Curriculum</g-link
           >
-            <g-link to="~/assets/CV-Lorena-Biologia-min.pdf" target="_blank"
-              >Curriculum</g-link
-            >
-          </button>
-          <button
-            class="mt-12 shadow button-btn sm:text-lg xl:text-xl focus:outline-none text-white px-4 py-2 sm:px-4 xl:py-2 xl:px-4 rounded mx-2"
-            type="button"
+        </button>
+        <button
+          class="mt-12 shadow button-btn sm:text-lg xl:text-xl focus:outline-none text-white px-4 py-2 sm:px-4 xl:py-2 xl:px-4 rounded mx-2"
+          type="button"
+        >
+          <g-link to="mailto:me@fagolambda.com" target="_blank"
+            >Envíame un correo</g-link
           >
-            <g-link to="mailto:me@fagolambda.com" target="_blank"
-              >Envíame un correo</g-link
-            >
-          </button>
-        </div>
+        </button>
       </div>
     </div>
   </Layout>
@@ -71,7 +75,7 @@ export default {};
 <style>
 .gradient-text-about {
   text-transform: uppercase;
-  background: linear-gradient(to right, #FFDB30 10%, #E79723 100%);
+  background: linear-gradient(to right, #ffdb30 10%, #e79723 100%);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
 }
