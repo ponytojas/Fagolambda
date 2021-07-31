fe7917931afb2cedca3647d5449271b14ef30292
ponytojas <daniel.villalobosdel@gmail.com>
2021-07-31 19:26:25 +0200

    🚑  Maybe this

--- gridsome.config.js
+++ gridsome.config.js
@@ -46,20 +46,7 @@ module.exports = {
         typeName: "TFG",
       },
     },
-    {
-      use: 'gridsome-plugin-purgecss',
-      // default options, the following will be included if you don't provide anything
-      options: {
-        content: [
-          './src/**/*.vue',
-          './src/**/*.js',
-          './src/**/*.jsx',
-          './src/**/*.pug',
-          './src/**/*.md'
-        ],
-        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-      }
-    }
+
   ],
   transformers: {
     remark: {
